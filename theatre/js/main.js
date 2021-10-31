30bd0537f831305c2432243196a6947f9e2479d5
SemeniukVlad <guaralit@gmail.com>
2021-10-31 15:55:11 +0200

    update

--- theatre/js/main.js
+++ theatre/js/main.js
@@ -3,16 +3,28 @@
 document.querySelector('.menu_btn').addEventListener('click', function(){
     let menu = document.querySelector('nav');
     menu.classList.toggle('menu_mob');
+    document.body.classList.toggle('scroll_prevention');
 });
 
-for (let item of document.querySelectorAll('nav a')){
+for (let item of document.querySelectorAll('.menu_mob a')){
     item.addEventListener('click', function(){
         let menu = document.querySelector('nav');
         menu.classList.toggle('menu_mob');
+        document.body.classList.toggle('scroll_prevention');
     });
 }
 
 document.querySelector('.close_btn').addEventListener('click', function(){
     let menu = document.querySelector('nav');
     menu.classList.toggle('menu_mob');
+    document.body.classList.toggle('scroll_prevention');
 });
+
+document.addEventListener('scroll', function(){
+    if (window.pageYOffset>0){
+        document.querySelector('header').classList.add('bg_darken');
+    } else {
+        document.querySelector('header').classList.remove('bg_darken');
+    }
+});
+
