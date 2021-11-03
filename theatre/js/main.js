e63757a453771d2deb3952c6eb7bc613a6dede1c
SemeniukVlad <guaralit@gmail.com>
2021-11-03 05:16:22 +0200

    update

--- theatre/js/main.js
+++ theatre/js/main.js
@@ -6,6 +6,14 @@ document.querySelector('.menu_btn').addEventListener('click', function(){
     document.body.classList.toggle('scroll_prevention');
 });
 
+for ( let item of document.querySelectorAll('nav a')){
+  item.addEventListener('click', function(){
+    let menu = document.querySelector('nav');
+    menu.classList.remove('menu_mob');
+    document.body.classList.remove('scroll_prevention');
+  });
+}
+
 document.querySelector('.close_btn').addEventListener('click', function(){
     let menu = document.querySelector('nav');
     menu.classList.toggle('menu_mob');
@@ -20,3 +28,59 @@ document.addEventListener('scroll', function(){
     }
 });
 
+const swiper = new Swiper('.pages', {
+    direction: 'horizontal',
+    loop: false,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 1,
+    spaceBetween: 30,
+});
+
+const swiper2 = new Swiper('.staging_swiper', {
+    slidesPerView: 'auto',
+    spaceBetween: 0,
+});
+
+  
+
+const swiper3 = new Swiper(".partners_swiper", {
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    slidesPerView: 1,
+    freeMode: true,
+    grid: {
+      fill: 'row',
+      rows: 2,
+    },
+    spaceBetween: 50,
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+        grid: {
+          fill: 'row',
+          rows: 2,
+        },
+        spaceBetween: 50,
+      },
+      768: {
+        slidesPerView: 3,
+        grid: {
+          fill: 'row',
+          rows: 2,
+        },
+        spaceBetween: 50,
+      },
+      1366: {
+        slidesPerView: 4,
+        grid: {
+          fill: 'row',
+          rows: 2,
+        },
+        spaceBetween: 50,
+      },
+    }
+});
